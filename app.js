fc2d4df9c6b577a9ab8bf97f3fefb3cf4b4baf71
stronkengineer <91441952+stronkengineer@users.noreply.github.com>
2023-12-09 22:12:45 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,86 +1,17 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-
-const session = require('express-session');
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-
-const User = require('./models/user');
-const account=require('./models/account')
-const service = require('./services/bankingService')
-const UserRoutes = require('./routes/userRoutes');
-const AccountRoutes = require('./routes/accountRoutes');
-
-const Database = require('./config/db');
-
+const express = require("express");
 const app = express();
-const port = 3000;
+const morgan = require("morgan");
+const clientsRouters = require("./routers/clients");
+const productsRouters = require("./routers/products");
+const accountsRouters = require("./routers/accounts");
+const transRoute=require("./routers/failedTransaction");
 
-// Connect to the database
-Database.initiateDBConnection();
+app.use(express.json());
+app.use(morgan('dev'));
 
-// Middleware for parsing JSON bodies
-app.use(bodyParser.json());
+app.use("/api/clients",clientsRouters);
+app.use("/api/accounts",accountsRouters);
+app.use("/api/trans",transRoute);
 
-// Initialize Passport and use sessions to keep track of login status
-app.use(session({ secret: 'sessionKey', resave: false, saveUninitialized: false }));
-app.use(passport.initialize());
-app.use(passport.session());
-
-// Use the LocalStrategy with Passport
-passport.use(new LocalStrategy(User.authenticate()));
-
-// Serialize and deserialize user information to support persistent login sessions
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
-
-// Use the user and account routes
-app.use('/users', UserRoutes);
-app.use('/accounts', AccountRoutes);
-
-app.listen(port, () => {
-    
-  console.log(`Server is running at http://localhost:${port}`);
-});
-
-async function someAsyncFunction() {
-    try {
-      const username = 'adamm';
-      const password = '1234';
-      const fullName = 'adam mohamed';
-  
-      newUser = await service.createUser(username, password, fullName);
-      console.log('User created successfully:', newUser);
-    } catch (error) {
-      console.error('Error creating user:', error.message);
-    }
-  }
-  someAsyncFunction();
-
-/*
- async function anotherLol(){
-    try{
-    const accountNumber = '1';
-    const initialBalance = 2035;
-    const userId = '6570ce2360b858cb515bbf7a';
-    
-    await service.createAccount(accountNumber, initialBalance, userId);
-    console.log('User created successfully:', newUser);
-    }
-    catch(error)
-    {
-        console.log('error creating acc',error);
-    }
- }
- anotherLol();
-*/
- 
-// Close the database connection on process exit
-process.on('SIGINT', async () => {
-  await Database.close();
-  mongoose.connection.close(() => {
-    console.log('MongoDB connection closed');
-    process.exit(0);
-  });
-});
+console.log("app is running");
+module.exports = app;
